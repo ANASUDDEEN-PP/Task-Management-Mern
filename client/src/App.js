102458be643fccfd5fc10ccfac11873855aac0c3
ana.su._ <98901363+ANASUDDEEN-PP@users.noreply.github.com>
2025-02-12 19:44:59 +0530

    Login and Register Page Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Login from './COMPONENTS/Login';
-import UserDashboard from './COMPONENTS/dashboard';
-import Signup from './COMPONENTS/Signup';
-import AddProduct from './COMPONENTS/addProduct';
-import ProductDetail from './COMPONENTS/productDetails';
-import SearchPage from './COMPONENTS/productSearch'
-import OrderPage from './COMPONENTS/order'
+import Login from './Pages/AuthPage';
 
 const Main = () => {
   return (
@@ -15,12 +9,6 @@ const Main = () => {
       <div>
         <Routes>
           <Route exact path="/" element={<Login />} />
-          <Route exact path="/user" element={<UserDashboard />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/add-product" element={<AddProduct />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/order" element={<OrderPage />} />
         </Routes>
       </div>
     </Router>
