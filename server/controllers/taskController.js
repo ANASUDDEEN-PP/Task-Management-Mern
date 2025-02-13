ace6e9fc8bf08f727ab5b71e97b7c371cf81e61e
ana.su._ <ppanasudheen@gmail.com>
2025-02-13 18:21:36 +0530

    all route added

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,6 +1,7 @@
 //import DB
 const taskModel = require('../models/taskModel');
 
+//Task Create Route
 exports.createTasks = async (req, res) => {
     const {TaskName, AssigneeName, Status, SubmitDate } = req.body;
     const taskData = {
@@ -22,4 +23,31 @@ exports.createTasks = async (req, res) => {
     } catch(err){
         console.log(err);
     }
+};
+
+//Task Edit
+exports.editTask = async (req, res) => {
+    //Task editing
+}
+
+//View all Tasks
+exports.viewAllTasks = async (req, res) => {
+    try{
+        const allDatas = await taskModel.find({})
+        .then(allDatas => {
+            res.status(200).json({
+                message: "All Datas Fetched",
+                allDatas
+            })
+        }) .catch(err=>{
+            console.log(err);
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//Delete Tasks
+exports.deleteTasks = async (req, res) => {
+    
 }
--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -5,5 +5,8 @@ const router =  express.Router();
 const taskController = require('../controllers/taskController');
 
 router.post('/create', taskController.createTasks);
+router.put('/edit', taskController.editTask);
+router.get('/view', taskController.viewAllTasks);
+router.delete('/delete', taskController.deleteTasks);
 
 module.exports = router;
